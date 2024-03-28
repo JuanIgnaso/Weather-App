46925ee0b3633230f2302818df972908540c93ea
JuanIgnaso <104755375+JuanIgnaso@users.noreply.github.com>
2024-03-28 20:54:51 +0100

    restructurado directorios

---
+++ src/js/getStartPoint.js
@@ -0,0 +1,19 @@
+/**
+ * Devuelve el punto de partida de la primera fila dependiendo de la hora del primer
+ * objeto del array.
+ *
+ * @param {number} hour
+ * @returns {number} index del punto de inicio
+ */
+
+
+
+
+function getStartingPoint(hour){
+    return [0,3,6,9,12,15,18,21].indexOf(hour);
+}
+
+
+export {getStartingPoint};
+
+
